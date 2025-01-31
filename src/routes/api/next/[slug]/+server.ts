8a997907bf31f256ee75f3a2c4c80c5ad3a270f6
byt3h3ad <adhirajd007@gmail.com>
2025-01-31 20:40:43 +0530

    fix: i am a dumbass

--- src/routes/api/next/[slug]/+server.ts
+++ src/routes/api/next/[slug]/+server.ts
@@ -5,10 +5,11 @@ import type { RequestHandler } from './$types';
 export const GET: RequestHandler = ({ params }) => {
 	const { slug } = params;
 	const id = Number(slug);
-	if (isNaN(id) || !Number.isInteger(id) || id < 1 || id > SITES.length) {
+	const length = SITES.length;
+	if (isNaN(id) || !Number.isInteger(id) || id < 1 || id > length) {
 		return error(400, 'Not a valid ID');
 	}
-	if (id === SITES.length) {
+	if (id === length) {
 		return redirect(307, SITES[0].url);
 	}
 	return redirect(307, SITES[id].url);
--- src/routes/api/prev/[slug]/+server.ts
+++ src/routes/api/prev/[slug]/+server.ts
@@ -5,11 +5,12 @@ import type { RequestHandler } from './$types';
 export const GET: RequestHandler = ({ params }) => {
 	const { slug } = params;
 	const id = Number(slug);
-	if (isNaN(id) || !Number.isInteger(id) || id < 1 || id > SITES.length) {
+	const length = SITES.length;
+	if (isNaN(id) || !Number.isInteger(id) || id < 1 || id > length) {
 		return error(400, 'Not a valid ID');
 	}
 	if (id === 1) {
-		return redirect(307, SITES[SITES.length - 1].url);
+		return redirect(307, SITES[length - 1].url);
 	}
-	return redirect(307, SITES[id - 1].url);
+	return redirect(307, SITES[id - 2].url);
 };
